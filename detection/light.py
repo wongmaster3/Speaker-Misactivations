82b45cfbb8292c23d650e256bbd2e52707d71103
Matt Wong <MattWong@Matts-MacBook-Pro-2.local>
2021-04-14 23:33:26 -0400

    First

--- detection/light.py
+++ detection/light.py
@@ -1,33 +1,20 @@
 import serial
 import time
 
-# Number of samples we are taking
-time_out = 1.0
-
 # Put own path for 'port' argument
 # Current wait for response from iot device is 5 seconds
-arduino = serial.Serial(port='/dev/cu.usbserial-141220', timeout=time_out, baudrate='9600')
+arduino = serial.Serial(port='/dev/cu.usbserial-145230', timeout=1.0, baudrate='9600')
 
-def log(filename, play_audio_func, audio, bitrate):
-    text = filename.split('.')[0]
-
-    # Play sound here
-    play_audio_func(audio, bitrate)
-
+def log(output_file):
     # Check if iot device activates on sound within 5 seconds
     # If it does, then log it or else do nothing
     on_state = str(arduino.readline())
+    # print("Hello")
     if on_state[1:] != "''":
-        # Since iot device activated, we want to deactivate the timeout for 'readline()'
         arduino.timeout = None
-
         start_time = str(time.time())
         off_state = str(arduino.readline())
         end_time = str(time.time())
+        arduino.timeout = 1.0
 
-        print(f'{text},{start_time},{end_time}')
-
-        # We want to set timeout back to 5 seconds since we want to play next noise
-        arduino.timeout = time_out
-    else:
-        print(f'{text},NA,NA')
+        output_file.write(f'{start_time},{end_time}\n')
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import multiprocessing
 import os
+import time
 from generate.play_audio import *
 from detection.light import *
 
-if __name__ == '__main__':
-    config = get_play_parser().parse_args()
+config = get_play_parser().parse_args()
+delay_between_words = config.delay
+light_file = open("./logs/" + config.device_name.lower() + "_light_activations.csv", "w")
+light_file.write('start_time,end_time\n')
+word_file = open("./logs/" + config.device_name.lower() + "_word_generations.csv", "w")
+word_file.write('word,start_time,end_time\n')
+generation_active_state = True
+
+def log_activations():
+    while generation_active_state:
+        log(light_file)
+    light_file.close()
+
+def generate_audio():
     root, _, filenames = next(os.walk(config.dir))
     for filename in filenames:
         filepath = os.path.join(root, filename)
         bitrate, audio = read(filepath)
         
-        log(filename, play_array, audio, bitrate)
+        # Play sound here
+        text = filename.split('.')[0]
+        start_time = str(time.time())
+        play_array(audio, bitrate)
+        end_time = str(time.time())
+        word_file.write(f'{text},{start_time},{end_time}\n')
+        time.sleep(delay_between_words)
+    
+    # Close files after logging everything
+    word_file.close()
+    generation_active_state = False
+
+if __name__ == '__main__':
+    jobs = []
+    p1 = multiprocessing.Process(name='log', target=log_activations)
+    p2 = multiprocessing.Process(name='generate', target=generate_audio)
+    p1.start()
+    p2.start()
