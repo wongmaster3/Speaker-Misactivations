eb8edfaf5c94f0e99ae6574f5faa34456b36621d
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2021-05-03 18:02:48 -0400

    added files for google q&a dataset processing

---
+++ generate/google_questions.py
@@ -0,0 +1,21 @@
+import argparse
+
+import jsonlines
+
+"""
+This script will create a "text source" for the n-gram model from the Google questions and
+answers dataset in JSONLines format (unzipped).
+
+The simplified source file is huge (over 4gb), so don't put it into git!
+"""
+
+parser = argparse.ArgumentParser(description='Extract questions from Q&A jsonlines as text')
+parser.add_argument('filename', help='unzipped JSONLines file (simplified)')
+parser.add_argument('output_filename', help='output filename of the text file')
+
+config = parser.parse_args()
+
+with jsonlines.open(config.filename) as reader, open(config.output_filename, 'w') as out:
+    for obj in reader:
+        question = obj['question_text']
+        out.write(f'{question}? ')
